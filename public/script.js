0a0a949fe38b7687c06c64de3c63e7dfd46da9d8
justyna7x <justynaczajka11@gmail.com>
2023-01-14 20:29:10 +0000

    Movie Finder

---
+++ public/script.js
@@ -0,0 +1,111 @@
+const tmdbKey = 'Your API key';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres =async () => {
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl+genreRequestEndpoint+requestParams;
+
+  try{
+   const response =  await fetch(urlToFetch);
+   if (response.ok){
+     const jsonResponse = await response.json();
+    // console.log(jsonResponse);
+     const genres = jsonResponse.genres;
+     //console.log(genres);
+     return genres
+   }
+
+  }
+  catch(error){
+    console.log(error)
+  }
+};
+
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie'
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  try{ const response =  await fetch(urlToFetch);
+   if (response.ok){
+     const jsonResponse = await response.json();
+    // console.log(jsonResponse);
+    const movies = jsonResponse.results;
+     // console.log(movies);   
+     return movies;
+   }
+  }
+  catch(error){
+    console.log(error)
+  }
+
+}
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+  try{
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      const movieInfo = jsonResponse;
+      //console.log(movieInfo);
+      return movieInfo;
+    }
+  } catch(error){
+      console.log(error);
+  };
+};
+const getMovieCast = async (movie)=>{
+    const movieId = movie.id;
+      const movieEndpoint = `/movie/${movieId}/credits`;
+      const requestParams = `?api_key=${tmdbKey}`;
+      const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+    try{
+    const response  =await fetch(urlToFetch);
+
+    if(response.ok){
+
+    const jsonResponse = await response.json();
+
+    const cast = jsonResponse.cast;
+
+    let castName = '';
+          for (let i = 0; i < 7; i++){
+            castName += cast[i].name + ', ';
+          };
+          return castName;
+    console.log(castName);
+    return castName;
+    }
+    }
+    catch(error){
+    console.log(error)
+    }
+}
+const addToLikedMovies = (movieInfo) => {
+  let likedMovies = '';
+  likedMovies += '❤️ '+movieInfo + (', ');
+  //console.log(likedMovies)
+  displayLikedMovies(likedMovies);
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie =async () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+ const movies = await getMovies();
+  const randomMovie = await getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  const cast = await getMovieCast(randomMovie);
+displayMovie(info,cast);
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
